9c2c75050010320a0d567f540f10641575974c51
jopedder <38110467+jopedder@users.noreply.github.com>
2026-03-19 15:07:19 +0000

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   const response = await fetch(
-    "https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services?f=json"
+    "https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/Special_Areas_of_Conservation_England/FeatureServer/0/query?"
   );
   const data = await response.json();
   return res.status(200).json(data);
