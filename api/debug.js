49eccc1cf6068b6a62535d3e125b218e74190cf9
jopedder <38110467+jopedder@users.noreply.github.com>
2026-03-20 09:00:21 +0000

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  const response = await fetch(
-    "https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/Special_Areas_of_Conservation_England/FeatureServer/0/query?"
-  );
-  const data = await response.json();
-  return res.status(200).json(data);
-}
